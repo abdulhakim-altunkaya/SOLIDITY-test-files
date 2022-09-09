30fb83c59c42d0934854f015f4ec20c85b173877
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-09-09 17:13:29 +0300

    many test files are added to the folder

---
+++ testPolygon/contracts/Test2.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.7;
+
+contract Test2 {
+    uint public data;
+
+    function setData(uint _data) external {
+        data = _data;
+    }
+}
+
